7b6f05522cf41ff89ac45f2753ca31961c29aad9
Jan Lorenz <jan.lorenz@scilifelab.se>
2024-06-14 18:54:33 +0200

    Changed the main page layout. Three tables were added from daisyUI and filled with basic content. The first table is correct but the second and third need content updates later. Early commit and PR to enable Sebastian starting dynamic frontend work on Main page in the upcoming Sprint.
    Signed-off-by: Jan Lorenz <jan.lorenz@scilifelab.se>

--- react-app/src/pages/HomePage.tsx
+++ react-app/src/pages/HomePage.tsx
@@ -1,86 +1,136 @@
 import { ReactElement } from 'react';
-// import CardComponent from "../components/CardComponent";
-import ImageCarouselAlternativeComponent from "../components/ImageCarouselAlternativeComponent";
 import { BODY_CLASSES, 
         // BUTTON_TYPE_ONE, 
         // H_1
     } from '../constants';
-// import { ICardConfig, ICardContent } from '../interfaces/types';
 import { TrackPageViewIfEnabled } from '../util/cookiesHandling';
 
 export default function HomePage(): ReactElement {
     TrackPageViewIfEnabled();
 
-    // var cardTitleClasses: string = "text-center text-base-100-content text-xl font-semibold";
-    // var cardTextClasses: string = "text-center";
-
-    // var cardConfig: { [id: string] : ICardConfig; } = {
-    //     'whiteCard': { 
-    //         cardClasses: "w-[40rem] bg-base-100 text-base-100-content bg-opacity-95 shadow-xl border-2 border-base-100", 
-    //         titleClasses: cardTitleClasses, 
-    //         subTitleClasses: "",
-    //         textClasses: cardTextClasses,
-    //         imgClasses: "", 
-    //         buttonClasses: "", 
-    //         buttonPlacement: "",  
-    //     },
-    //     'blackCard': { 
-    //         cardClasses: "bg-neutral bg-opacity-95 rounded-[10px] shadow border-2", 
-    //         titleClasses: cardTitleClasses, 
-    //         subTitleClasses: "",
-    //         textClasses: cardTextClasses,
-    //         imgClasses: "",
-    //         buttonClasses: BUTTON_TYPE_ONE, 
-    //         buttonPlacement: "justify-center", 
-    //     }
-    // };
-
-    // var cardContent: { [id: string] : ICardContent } = {
-    //     'whiteCard1': {
-    //         title: "",
-    //         subTitle: "",
-    //         text: "In development", 
-    //         buttonText: "",
-    //         imageSrc: "",
-    //         imageAlt: "",
-    //     },
-    //     'blackCard1': {
-    //         title: "Data sources", 
-    //         subTitle: "",
-    //         text: "In development", 
-    //         buttonText: "Sign In",
-    //         imageSrc: "",
-    //         imageAlt: "",
-    //     },
-
-    // };
-
     return (
         <div>
             <div className={BODY_CLASSES}>
-                <ImageCarouselAlternativeComponent />
-                {
-  /* Commented out until the team has sufficient time to fill the cards with useful content
-                <div className="flex flex-row justify-between">
-                    <div className="flex flex-col space-y-1.5">
-                        <h1 className={H_1}>Latest News</h1>
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['blackCard']} cardContent={cardContent['blackCard1']} />
-                    </div>
-                    <div className="flex flex-col space-y-1.5">
-                        <h1 className={H_1}>Latest News</h1>
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['whiteCard']} cardContent={cardContent['whiteCard1']} />
-                        <CardComponent cardConfig={cardConfig['blackCard']} cardContent={cardContent['blackCard1']} />
-                    </div>
+                // DaisyUI item: Table with pinned-rows. Add hover effect and active row effect as displayed in the first two tables in DaisyUI. Table Header needs to be highlighted differently in all tables.
+                <div className="overflow-x-auto h-96">
+                    <table className="table table-pin-rows">
+                    <thead>
+                        <tr>
+                        <th className='font-bold'>Gene</th>
+                        </tr>
+                    </thead>
+                    <thead>
+                        <tr>
+                        <th>IGH</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>IGHV</td></tr>
+                        <tr><td>IGHD</td></tr>
+                        <tr><td>IGHJ</td></tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th>TRG</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>TRGV</td></tr>
+                        <tr><td>TRGJ</td></tr>
+                        <tr><td>TRG constant</td></tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th>IGK</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>IGKJ</td></tr>
+                        <tr><td>IGKL</td></tr>
+                        <tr><td>IGK constant</td></tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th>IGL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>IGL constant</td></tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th>TRA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>TRAV</td></tr>
+                        <tr><td>TRAJ</td></tr>
+                        <tr><td>TRA constant</td></tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th>TRD</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>TRDV</td></tr>
+                        <tr><td>TRDJ</td></tr>
+                        <tr><td>TRDD</td></tr>
+                        <tr><td>TRD constant</td></tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th>TRB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>TRBV</td></tr>
+                        <tr><td>TRBJ</td></tr>
+                        <tr><td>TRBD</td></tr>
+                        <tr><td>TRB constant</td></tr>
+                    </tbody>
+                    </table>
                 </div>
-  */
-}
+
+
+                // DaisyUI item: Table with pinned-rows. Add hover effect and active row effect as displayed in the first two tables in DaisyUI.
+                // This table should be reset every time the first table row is changed / clicked.
+                <div className="overflow-x-auto h-96">
+                    <table className="table table-pin-rows">
+                    <thead>
+                        <tr>
+                        <th className='font-bold'>Subtype</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>1-2</td></tr>
+                        <tr><td>3-4</td></tr>
+                        <tr><td>???</td></tr>
+                    </tbody>
+                    </table>
+                </div>
+
+                
+                // DaisyUI item: Table with pinned-rows. Add hover effect and active row effect as displayed in the first two tables in DaisyUI.
+                // This table should be reset every time the first or second table is changed / clicked.
+                <div className="overflow-x-auto h-96">
+                    <table className="table table-pin-rows">
+                    <thead>
+                        <tr>
+                        <th className='font-bold'>Allele</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>*02</td></tr>
+                        <tr><td>*03</td></tr>
+                        <tr><td>*04</td></tr>
+                    </tbody>
+                    </table>
+                </div>
+
+                // Plots should be empty and only display plots when the user selects a row in all three tables.
+
+
             </div>
         </div>
     );
