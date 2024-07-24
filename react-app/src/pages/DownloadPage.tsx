b144d8d914327f3d7eb94d155676ef4986011034
Jan Lorenz <jan.lorenz@scilifelab.se>
2024-07-24 14:54:36 +0200

    Changed border-radius of download button
    Signed-off-by: Jan Lorenz <jan.lorenz@scilifelab.se>

--- react-app/src/pages/DownloadPage.tsx
+++ react-app/src/pages/DownloadPage.tsx
@@ -158,7 +158,7 @@ export default function DownloadPage(): ReactElement {
 
       <div className="flex justify-center">
         <a href="" rel="noopener noreferrer">
-          <div className="bg-gradient-to-r from-[rgba(67,133,139)] to-primary text-primary-content text-lg tracking-wide flex gap-4 justify-center items-center w-96 h-14 font-extrabold rounded-2xl shadow-inner backdrop-blur-2xl transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:opacity-90">
+          <div className="bg-gradient-to-r from-[rgba(67,133,139)] to-primary text-primary-content text-lg tracking-wide flex gap-4 justify-center items-center w-96 h-14 font-extrabold rounded-3xl shadow-inner backdrop-blur-2xl transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:opacity-90">
             Download
             <svg
               xmlns="http://www.w3.org/2000/svg"
