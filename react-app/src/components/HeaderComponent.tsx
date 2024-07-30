b1c5f02bdbf61b128af0da683ac2e6f23482db92
Jan Lorenz <jan.lorenz@scilifelab.se>
2024-07-30 12:33:44 +0200

    Basic redesign of header.
    Signed-off-by: Jan Lorenz <jan.lorenz@scilifelab.se>

--- react-app/src/components/HeaderComponent.tsx
+++ react-app/src/components/HeaderComponent.tsx
@@ -3,7 +3,6 @@ import { ILink } from "../interfaces/types";
 import { LINK_CLASSES } from "../constants";
 
 export default function HeaderComponent() {
-
   let links: { [id: string]: ILink } = {
     l1: { text: "Download", classes: LINK_CLASSES, link: "download" },
     l2: { text: "Plot alleles", classes: LINK_CLASSES, link: "plot" },
@@ -25,35 +24,67 @@ export default function HeaderComponent() {
         </p>
       </div>
       <div className="bg-gradient-to-b from-primary to-secondary">
-        <div className="text-primary-content px-36 pt-16 py-4 2xl:max-w-screen-2xl 2xl:mx-auto">
-          <div className="navbar">
+        <div className="px-4 py-4 lg:px-36 lg:pt-16 2xl:max-w-screen-2xl 2xl:mx-auto">
+          <div className="navbar text-primary-content">
             <div className="navbar-start">
               <Link to="/">
                 <div className="font-bold text-center">
                   <p className="text-2xl">KIARVA</p>
-                  <span className="whitespace-nowrap text-xl">
+                  <span className="lg:whitespace-nowrap text-xl">
                     KI Adaptive Immune Receptor Gene Variant Atlas
                   </span>
                 </div>
               </Link>
             </div>
-            <div className="navbar-center lg:flex">
+            <div className="navbar-center hidden lg:flex">
               <ul className="menu menu-horizontal text-lg">
                 {Object.keys(links).map((key) => (
                   <li key={key} className="ml-1.5 max-w-36">
-                    {
+                    <NavLink
+                      className={links[key].classes}
+                      to={links[key].link}
+                    >
+                      {links[key].text}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="navbar-end lg:hidden">
+              <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-base-content"
+                >
+                  {Object.keys(links).map((key) => (
+                    <li key={key}>
                       <NavLink
                         className={links[key].classes}
                         to={links[key].link}
                       >
                         {links[key].text}
                       </NavLink>
-                    }
-                  </li>
-                ))}
-              </ul>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <div className="navbar-end hidden 2xl:block"></div>
           </div>
         </div>
       </div>
--- react-app/src/constants.ts
+++ react-app/src/constants.ts
@@ -2,15 +2,19 @@
 export const H_1: string = "text-left text-black text-[40px] font-semibold";
 export const H_2: string = "text-left text-black text-2xl font-semibold";
 
-export const BUTTON_TYPE_ONE: string = 'btn bg-fuchsia-950 text-white hover:bg-fuchsia-800 active:bg-fuchsia-900 focus:outline-none focus:ring focus:ring-fuchsia-300';
-export const BUTTON_TYPE_TWO: string = 'btn bg-gray-950 text-white hover:bg-gray-800 active:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-300';
+export const BUTTON_TYPE_ONE: string =
+  "btn bg-fuchsia-950 text-white hover:bg-fuchsia-800 active:bg-fuchsia-900 focus:outline-none focus:ring focus:ring-fuchsia-300";
+export const BUTTON_TYPE_TWO: string =
+  "btn bg-gray-950 text-white hover:bg-gray-800 active:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-300";
 
-export const BODY_CLASSES: string = "bg-base-100 space-y-8 py-4 px-36 pb-28 2xl:max-w-screen-2xl 2xl:mx-auto";
+export const BODY_CLASSES: string =
+  "bg-base-100 space-y-8 py-4 px-36 pb-28 2xl:max-w-screen-2xl 2xl:mx-auto";
 
-export const LINK_CLASSES: string = 'link link-hover';
+export const LINK_CLASSES: string = "link link-hover";
 
-export const backendAPI =   window.location.origin === 'http://localhost:5173' ? 
-                            (import.meta.env.VITE_BACKEND_API_URL ?
-                                import.meta.env.VITE_BACKEND_API_URL :
-                                'https://kiarva.scilifelab-2-dev.sys.kth.se/api/') : 
-                            window.location.origin + '/api/';
+export const backendAPI =
+  window.location.origin === "http://localhost:5173"
+    ? import.meta.env.VITE_BACKEND_API_URL
+      ? import.meta.env.VITE_BACKEND_API_URL
+      : "https://kiarva.scilifelab-2-dev.sys.kth.se/api/"
+    : window.location.origin + "/api/";
