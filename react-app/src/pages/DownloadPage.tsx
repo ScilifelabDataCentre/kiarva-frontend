a5bc18fc9301bb81b80f3cda825c5b15f34445fe
Jan Lorenz <jan.lorenz@scilifelab.se>
2024-07-31 14:57:16 +0200

    Changed the Download page to be responsive and look nice on mobile.
    Signed-off-by: Jan Lorenz <jan.lorenz@scilifelab.se>

--- react-app/src/pages/DownloadPage.tsx
+++ react-app/src/pages/DownloadPage.tsx
@@ -2,9 +2,9 @@ import { ReactElement, useState } from "react";
 import { backendAPI, BODY_CLASSES, H_1 } from "../constants";
 import { TrackPageViewIfEnabled } from "../util/cookiesHandling";
 import DownloadBoxComponent from "../components/DownloadBoxComponent";
-import axios from 'axios';
-import fileDownload from 'js-file-download';
-import JSZip from 'jszip';
+import axios from "axios";
+import fileDownload from "js-file-download";
+import JSZip from "jszip";
 
 export default function DownloadPage(): ReactElement {
   // Track the page view for analytics if enabled in the application settings
@@ -20,54 +20,61 @@ export default function DownloadPage(): ReactElement {
   const [traSelectionArray, setTraSelectionArray] = useState<string[]>([]);
 
   async function downloadGeneFasta(gene: string) {
-      let fastaType = fastaTypeSelected === 'coding' ? '' : fastaTypeSelected + '/';
-      let fastaEndpoint = backendAPI + "fasta/" + fastaType + gene;
-      await axios.get(fastaEndpoint, {
-          headers: {
-            "Content-Type": 'attachment'
-          }
-         })
-          .then(response => {
-              let responseData: Blob = response.data;
-              fileDownload(responseData, gene + '-' + fastaTypeSelected + '.fasta');
-          })
-          .catch(response => console.log(response.error));
+    let fastaType =
+      fastaTypeSelected === "coding" ? "" : fastaTypeSelected + "/";
+    let fastaEndpoint = backendAPI + "fasta/" + fastaType + gene;
+    await axios
+      .get(fastaEndpoint, {
+        headers: {
+          "Content-Type": "attachment",
+        },
+      })
+      .then((response) => {
+        let responseData: Blob = response.data;
+        fileDownload(responseData, gene + "-" + fastaTypeSelected + ".fasta");
+      })
+      .catch((response) => console.log(response.error));
   }
 
   async function downloadGeneFastaZip(genes: string[]) {
-      let zip = new JSZip();
-      let gene: string;
-      let fastaType = fastaTypeSelected === 'coding' ? '' : fastaTypeSelected + '/';
-      for (gene of genes) {
-          let fastaEndpoint = backendAPI + "fasta/" + fastaType + gene;
-          await axios.get(fastaEndpoint, {
-              headers: {
-              "Content-Type": 'attachment'
-              }
-          })
-              .then(response => {
-                  let responseData: Blob = response.data;
-                  zip.file(gene + '-' + fastaTypeSelected + '.fasta', responseData);
-              })
-              .catch(response => console.log(response.error));
+    let zip = new JSZip();
+    let gene: string;
+    let fastaType =
+      fastaTypeSelected === "coding" ? "" : fastaTypeSelected + "/";
+    for (gene of genes) {
+      let fastaEndpoint = backendAPI + "fasta/" + fastaType + gene;
+      await axios
+        .get(fastaEndpoint, {
+          headers: {
+            "Content-Type": "attachment",
+          },
+        })
+        .then((response) => {
+          let responseData: Blob = response.data;
+          zip.file(gene + "-" + fastaTypeSelected + ".fasta", responseData);
+        })
+        .catch((response) => console.log(response.error));
+    }
+    zip.generateAsync({ type: "blob" }).then(
+      function (blob) {
+        fileDownload(blob, "kiarva-" + fastaTypeSelected + "-fastas.zip");
+      },
+      function (err) {
+        console.log(err);
       }
-      zip.generateAsync({type:"blob"}).then(function (blob) {
-          fileDownload(blob, "kiarva-"+fastaTypeSelected+"-fastas.zip");
-      }, function (err) {
-          console.log(err)
-      })
+    );
   }
 
   function handleDownload() {
     let selectionArr = ighSelectionArray.concat(
       igkSelectionArray,
       iglSelectionArray,
-      traSelectionArray);
-    
+      traSelectionArray
+    );
+
     if (selectionArr.length === 1) {
       downloadGeneFasta(selectionArr[0]);
-    }
-    else if (selectionArr.length > 1) {
+    } else if (selectionArr.length > 1) {
       downloadGeneFastaZip(selectionArr);
     }
   }
@@ -90,18 +97,15 @@ export default function DownloadPage(): ReactElement {
             d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           ></path>
         </svg>
-        <div className="flex flex-col">
-          <label className="font-bold">How to download</label>
-          <span>
-            Start by choosing the type of fasta file you want. Then, select the
-            gene segment and/or individual genes you want to download by ticking
-            the appropriate boxes—ticking a gene segment will automatically
-            select all individual genes within that chain. After making your
-            selections, click the "Download" button. This will generate and
-            start a download of your chosen file(s). If you have selected more
-            than one file, a .zip file wil be generated.
-          </span>
-        </div>
+        <span className="text-sm lg:text-base">
+          Start by choosing the type of fasta file you want. Then, select the
+          gene segment and/or individual genes you want to download by ticking
+          the appropriate boxes—ticking a gene segment will automatically select
+          all individual genes within that chain. After making your selections,
+          click the "Download" button. This will generate and start a download
+          of your chosen file(s). If you have selected more than one file, a
+          .zip file wil be generated.
+        </span>
       </div>
       <div className={H_1}>Fasta type</div>
       <div className="divider !my-0"></div>
@@ -112,15 +116,18 @@ export default function DownloadPage(): ReactElement {
             className="flex rounded-md px-2 py-2 my-3 transition-all duration-300 hover:bg-neutral cursor-pointer"
             onClick={() => setFastaTypeSelected("coding")}
           >
-            <input type="radio" name="fastaRadio" className="radio" defaultChecked/>
+            <input
+              type="radio"
+              name="fastaRadio"
+              className="radio"
+              defaultChecked
+            />
             <span className="pl-2">Coding sequence</span>
           </label>
 
           <label
             className="flex rounded-md px-2 py-2 my-3 transition-all duration-300 hover:bg-neutral cursor-pointer"
-            onClick={() =>
-              setFastaTypeSelected("genomic")
-            }
+            onClick={() => setFastaTypeSelected("genomic")}
           >
             <input type="radio" name="fastaRadio" className="radio" />
             <span className="pl-2">Genomic sequence with flanking regions</span>
@@ -139,7 +146,7 @@ export default function DownloadPage(): ReactElement {
 
       <div className={H_1}>BCR</div>
       <div className="divider !my-0"></div>
-      <div className="flex justify-start gap-16">
+      <div className="flex flex-col lg:flex-row justify-start gap-4 lg:gap-16">
         <DownloadBoxComponent
           geneSegment="IGH"
           geneObjectArray={[
@@ -172,7 +179,7 @@ export default function DownloadPage(): ReactElement {
 
       <div className={H_1}>TCR</div>
       <div className="divider !my-0"></div>
-      <div className="flex justify-start gap-16">
+      <div className="flex flex-col lg:flex-row justify-start gap-4 lg:gap-16">
         <DownloadBoxComponent
           geneSegment="TRA"
           geneObjectArray={[
@@ -184,9 +191,9 @@ export default function DownloadPage(): ReactElement {
         ></DownloadBoxComponent>
       </div>
 
-      <div className="flex justify-center">
+      <div className="flex justify-center py-8 lg:py-0">
         <button onClick={handleDownload}>
-          <div className="bg-gradient-to-r from-[rgba(67,133,139)] to-primary text-primary-content text-lg tracking-wide flex gap-4 justify-center items-center w-96 h-14 font-extrabold rounded-3xl shadow-inner backdrop-blur-2xl transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:opacity-90">
+          <div className="bg-gradient-to-r from-[rgba(67,133,139)] to-primary text-primary-content text-lg tracking-wide flex gap-4 justify-center items-center w-64 lg:w-96 h-14 font-extrabold rounded-3xl shadow-inner backdrop-blur-2xl transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:opacity-90">
             Download
             <svg
               xmlns="http://www.w3.org/2000/svg"
