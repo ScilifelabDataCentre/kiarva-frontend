55198e3a6b9c8d884dc7624de53f0089df7e8795
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-10-09 21:22:21 +0200

    frontend to backend password check working

--- next-app/src/app/password/page.tsx
+++ next-app/src/app/password/page.tsx
@@ -1,33 +1,44 @@
 'use client';
 
 import { ChangeEvent, ReactElement, useState } from 'react';
-import { BODY_CLASSES } from '@/constants'
+import { backendAPI, BODY_CLASSES } from '@/constants'
 import axios from 'axios';
 
 export default function PasswordPage(): ReactElement {
     const [inputField, setInputField] = useState("");
+    const [correctPassword, setCorrectPassword] = useState("");
 
-    const backend_password_uri: string = process.env.BACKEND_API_URL + 'passvalidation';
+    const backend_password_uri: string = backendAPI + 'checkapikey';
 
     function handleChange(e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) { 
         setInputField(e.target.value);
     }
 
     async function handleSubmit(): Promise<void> {
+        console.log("wot");
+        const config = {
+            headers: {
+                'X-api-key': inputField,
+            }
+          }
+
         await axios
-            .get(backend_password_uri)
+            .get(backend_password_uri,config)
             .then((response) => {
-                //
+                console.log(response);
+                setCorrectPassword("Password correct, unlocking full functionality.");
             })
-            .catch((response) => console.log(response.error));
-        console.log("The password is:" + inputField);
+            .catch((response) => {
+                setCorrectPassword("Password incorrect, try again or continue with restricted version.");
+            });
+        // console.log("The password is:" + inputField);
     }
 
     return (
         <div className={BODY_CLASSES}>
             <div className="flex flex-col">
                 <input 
-                    type="text"
+                    type="password"
                     name="password"
                     placeholder="Type here" 
                     className="input bg-white input-bordered border-neutral w-full max-w-xs"
@@ -36,6 +47,7 @@ export default function PasswordPage(): ReactElement {
                     required 
                 />
                 <button onClick={handleSubmit} className="btn btn-wide bg-fuchsia-950 text-white hover:bg-fuchsia-800 active:bg-fuchsia-900 focus:outline-none focus:ring focus:ring-fuchsia-300">Submit</button>
+                <p>{correctPassword}</p>
             </div>
         </div>
     );
