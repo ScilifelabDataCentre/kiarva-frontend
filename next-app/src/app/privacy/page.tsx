0d26995f1bd875f1a99149b1949768f6d991667a
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-11-07 09:19:50 +0100

    Realized that the opt out of matomo tracking did not exist yet, added it

--- next-app/src/app/privacy/page.tsx
+++ next-app/src/app/privacy/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 
-import { ReactElement } from "react";
-import { BODY_CLASSES, H_1 } from "@/constants";
-import { TrackPageViewIfEnabled } from "@/util/cookiesHandling";
+import { ReactElement, useState } from "react";
+import { BODY_CLASSES, BUTTON_TYPE_ONE, H_1 } from "@/constants";
+import { trackingDisabled, TrackPageViewIfEnabled } from "@/util/cookiesHandling";
 import Link from "next/link";
+import { deleteCookie, setCookie } from "cookies-next";
 
 export default function PrivacyPage(): ReactElement {
   TrackPageViewIfEnabled();
 
+  const optInOrOutTextActive = (isTrackingEnabled: boolean): string[] => {
+    if (isTrackingEnabled) {
+        return ["Click on the button to the right to opt out of tracking", "Opt Out"]
+    }
+    else {
+        return ["Click on the button to the right to opt in to tracking", "Opt In"]
+    }
+  }
+
+  const [ optInText, setOptInText ] = useState(optInOrOutTextActive(!trackingDisabled()))
+
+  const handleOptOut = () => {
+      trackingDisabled() ? 
+          deleteCookie('trackingDisabled') :
+          setCookie('trackingDisabled', 'true', { maxAge: 365 })
+      setOptInText(optInOrOutTextActive(!trackingDisabled()))
+  };
+  
   return (
     <>
       <div className={BODY_CLASSES}>
@@ -26,6 +45,13 @@ export default function PrivacyPage(): ReactElement {
           and of the Council of 27 April 2016, the General Data Protection
           Regulation.
         </p>
+        <div role="alert" className="alert bg-neutral text-neutral-content">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>{optInText[0]}</span>
+          <div className="space-x-2">
+              <button onClick={handleOptOut} className={BUTTON_TYPE_ONE}>{optInText[1]}</button>
+          </div>
+        </div>
         <div className="divider">Links to Other Sites</div>
         <p>
           Our Service may contain links to other sites. If you click on a
