00466e80c73ab41a70e3125406a8b3feff904e5b
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-11-26 18:17:21 +0100

    Fixed up the appearance of MSA page a bit. Fixed some bugs with the UI colors.

---
+++ next-app/src/app/msa/page.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import DisclaimerPopupComponent from '@/components/DisclaimerPopupComponent';
+import MSAPlotPageComponent from '@/components/MSAPlotPageComponent';
+import { BODY_CLASSES, H_1 } from '@/constants';
+import { hasCookie } from 'cookies-next';
+import { ReactElement } from 'react';
+
+// Main function to render the PlotPage component
+export default function AminoAcidPlotPage(): ReactElement {
+    function setIsDisclaimerPopupOpen(arg0: boolean): void {
+        console.log("Disclaimer popup button not implemented.")
+        }
+  return (
+    <div className={BODY_CLASSES}>
+      <h1 className={H_1}>Multiple sequence alignment of allele sequences translating to the same amino acid</h1>
+        {!hasCookie('password') &&
+        <button
+          className="bg-warning text-warning-content text-base lg:text-lg flex gap-2 justify-center items-center px-4 order-first lg:px-0 w-full h-12 font-bold rounded-3xl shadow-inner backdrop-blur-2xl transform transition duration-300 ease-in-out hover:opacity-90"
+          onClick={() => setIsDisclaimerPopupOpen(true)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          Disclaimer
+        </button>}
+        {!hasCookie('password') && setIsDisclaimerPopupOpen && (
+          <DisclaimerPopupComponent
+            onClose={() => setIsDisclaimerPopupOpen(false)}
+            explanation="This page is fully developed and allows you to explore its
+                      design and functionality. However, the underlying data has
+                      not been officially published yet. Therefore, we can currently only showcase three plots for demonstration purposes."
+          />
+        )}
+
+        <div className="alert">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 shrink-0 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span className="text-sm lg:text-base">
+            This page allows users to generate multiple sequence alignments for alleles in 
+            the KI Adaptive Immune Receptor Gene Variant Atlas. You can select
+            from various dropdowns to filter by gene segment, gene type, gene,
+            and allele.
+          </span>
+        </div>
+        <MSAPlotPageComponent />
+    </div>
+  );
+}
