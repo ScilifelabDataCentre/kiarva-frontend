a2510e108e401eba5fd33be41b8e48002d9b1dd5
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2025-04-09 09:12:10 +0200

    fix: removed unnecessary extra component SpinnerWithText, moved content to Loading.tsx

--- next-app/src/components/Loading.tsx
+++ next-app/src/components/Loading.tsx
@@ -1,5 +1,10 @@
-import SpinnerWithText from '@/components/SpinnerWithText';
+import React from 'react';
+import { Spinner } from '@/components/ui/spinner';
 
 export default function Loading() {
-    return <SpinnerWithText />
+    return (
+      <div className="content-center">
+        <Spinner>Loading</Spinner>
+      </div>
+    );
   }
