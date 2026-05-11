a88846486e25856fbef82b98b5da50658e0eae54
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2026-05-11 14:22:08 +0200

    Fixed issue where 'Allele', 'Sequence' and divider bar were not sticky on sequencesearch page, disappearing to the left when scrolling.

--- next-app/src/components/SequenceSearchComponent.tsx
+++ next-app/src/components/SequenceSearchComponent.tsx
@@ -56,11 +56,11 @@ const SequenceSearchComponent: React.FC<SequenceSearchProps> = ({
       className="flex flex-col items-start max-w-full overflow-x-auto"
       aria-label="Sequence search results"
     >
-      <div className="flex flex-row w-full pb-8">
+      <div className="flex flex-row w-full pb-8 sticky left-0">
         <h2 className="w-62.5 font-bold">Allele</h2>
         <h2 className="font-bold">Sequence</h2>
       </div>
-      <div className="grow w-full border-t border-secondary/30 pb-4"></div>
+      <div className="grow w-full border-t border-secondary/30 pb-4 sticky left-0"></div>
       {sequenceData.map((seq) => (
         <div key={seq.allele} className="flex flex-row" role="row">
           {/* Allele name on the left */}
